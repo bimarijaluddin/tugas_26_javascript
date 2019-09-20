5c2190e9c5c79e1279f15bd6c02570bbfc685eb6
bima rijaluddin <bimarijal@gmail.com>
2019-09-20 14:26:19 +0800

    Tugas 26 Javascript
    Tugas dari niomic

---
+++ tugas26.js
@@ -0,0 +1,11 @@
+console.log("Angka " + 2 + " NOT Infinity.");
+console.log("Angka " + 39 + " NOT Infinity.");
+console.log("Angka " + 76 + " NOT Infinity.");
+console.log("Angka " + 50 + " NOT Infinity.");
+console.log("Angka " + 9 + " NOT Infinity.");
+console.log("Angka " + 7 + " NOT Infinity.");
+console.log("Angka " + 41 + " NOT Infinity.");
+console.log("Angka " + 2 + " NOT Infinity.");
+console.log("Angka " + 24 + " NOT Infinity.");
+console.log("Angka " + 1 + " NOT Infinity.");
+console.log("Angka " + 16 + " NOT Infinity.");
